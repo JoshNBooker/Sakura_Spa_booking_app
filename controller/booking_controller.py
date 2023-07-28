c3be7f2c9297313de94629217d38cb9b736a2ca7
Josh Booker <josh.booker2@gmail.com>
2023-07-28 12:48:17 +0100

    began working on diary page for each customer

--- controller/booking_controller.py
+++ controller/booking_controller.py
@@ -10,7 +10,27 @@ def home_page():
 
 @booking_blueprint.route('/bookings')
 def display_bookings():
-    return render_template('bookings/diary.jinja', title = 'Diary')
+    bookings = Booking.query.all()
+    customers = Customer.query.all()
+    return render_template('bookings/diary.jinja', title = 'Diary', bookings=bookings, customers=customers)
+
+@booking_blueprint.route('/bookings', methods=["POST"])
+def submit_new_booking():
+    date_time = request.form["date_time"]
+    customer_id = request.form["customer"]
+    treatment_id = request.form["treatment"]
+    new_booking = Booking(date_time = date_time,customer_id = customer_id,treatment_id = treatment_id)
+    db.session.add(new_booking)
+    db.session.commit()
+    return redirect('/bookings')
+
+@booking_blueprint.route('/bookings/<id>')
+def show_customers_bookings(id):
+    id = request.form['customer']
+    customer_id = id
+    bookings = bookings.query.get(customer_id)
+    return render_template('customer_bookings.jinja', id=id)
+
 
 @booking_blueprint.route('/bookings/new')
 def new_booking_form():
