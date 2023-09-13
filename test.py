6532fa6e2bd39c74ab62710efb42f502105deb69
Josh Booker <josh.booker2@gmail.com>
2023-09-13 15:37:10 +0100

    added readme

--- test.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask, render_template, redirect, Blueprint, request
-from app import db
-from models import Customer, Treatment, Booking,Room
-from datetime import datetime, timedelta
-from controller.booking_controller import check_booking
